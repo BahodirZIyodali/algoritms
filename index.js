df0547139131fe7bb867a489a9b226fd6f54bbd8
bakhadir <bohodirziyodaliyev@gmail.com>
2023-02-22 12:30:55 +0500

    ok

--- index.js
+++ index.js
@@ -333,9 +333,10 @@ const number =[0,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
 let newNum=[]
   number.forEach((e)=>{
     if(!newNum.includes(e)){
-newNum.push(e)
+   newNum.push(e)
     } else{
       newNum.push('_')
     }
   })
   console.log(newNum.sort())
+
