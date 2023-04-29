5873857962a66070c59a124cf0e411eda0da2229
Bahodir_Ziyodaliyev <113345966+BahodirZIyodali@users.noreply.github.com>
2023-04-29 08:49:42 +0500

    Update script.ts

--- script.ts
+++ script.ts
@@ -5,6 +5,7 @@
 //     return nums;
 //   }
 //   console.log(swap([1, 2, 10]));
+
   
 //   function command(a: number[], b: number[]): boolean {
 //     if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) {
